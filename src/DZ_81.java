da914f0b281ade3ace4f5423b594fdd9609e0c9b
rak15 <rak1529916@mail.ru>
2023-05-21 15:17:01 +0300

    1.1

--- src/DZ_81.java
+++ src/DZ_81.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class DZ_81 {
+import java.util.concurrent.Semaphore;
+
+public class DZ_81 {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(1);
+        Gocar gocar = new Gocar(14,semaphore);
+        Outcar outcar3 = new Outcar(4,semaphore);
+        Gocar gocar1 = new Gocar(2,semaphore);
+        Outcar outcar = new Outcar(8,semaphore);
+        Outcar outcar2 = new Outcar(2,semaphore);
+    }
 }
